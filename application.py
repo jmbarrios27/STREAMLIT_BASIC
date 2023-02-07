02094e980bc7aea560c2c86b0f87d158c6e2c096
José Max Barrios <55581483+jmbarrios27@users.noreply.github.com>
2023-02-07 15:30:54 -0500

    Add files via upload

---
+++ application.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+
+def main():
+    st.title("Hi Anber! Welcome to Streamlit!")
+
+    st.write("My first DataFrame")
+
+    st.write(
+    pd.DataFrame({
+        'A': [1, 5, 9, 7],
+        'B': [3, 2, 4, 8]
+    })
+    )
+
+
+if __name__ == "__main__":
+    main()
